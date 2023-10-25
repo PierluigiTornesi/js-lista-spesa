768a8d47a653b7e7f69d93a625f6fed903db9997
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2023-10-25 13:45:31 +0200

    add: stampa nell'html

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
+//prendo il div dell'html per metterci dentro al lista
+const listaElem = document.querySelector(".listaSpesa");
+console.log(listaElem);
+//variabile che conterrá la lista dei prodotti da inserire nel div
+let listaProd = "";
+
 //array contennete la lista della spesa
 const arraySpesa = ["pomodori","lette","biscotti","pollo","acqua"]
 //variabile per la lunghezza dell'array
 let lunghezza = arraySpesa.length;
 //indice per spostarmi dentro l'array
 let i = 0;
+//variabile per salvare il prodotto
+let prodotto = "";  
 //ciclo per stampare cosa c'é dentro l'array
 while(lunghezza > 0){
     //stampo il valore alla posizione dell'indice
     console.log(arraySpesa[i]);
+    //salvo il prodotto
+    prodotto = arraySpesa[i];
     //incremento l'indice
     i++;
     //decremento la lunghezza
     lunghezza--;
+    //scrivo nella lista il div contenente il prodotto
+    listaProd += `<div><p>Il prodotto alla posizione numero ${i} della lista é "${prodotto}"</p></div>`
 }
+//aggiungo all'html tutta la lista dei prodotti
+listaElem.innerHTML = listaProd;
--- readme.md
+++ readme.md
@@ -15,10 +15,16 @@ Come posso ciclare su un array con il ciclo while?
 1. Array con la lista della spesa
 2. Lunghezza array
 3. Indice array
+4. Variabile per salvare il prodotto
+
 **Logica**
+
 1. Ciclo while con condizione contenente la lunghezza dell'array > 0
 2. Nel ciclo stampo il prodotto che si trova nell'array nella posizione dell'indice
-3. Incremento l'indice
-4. Decremento la lunghezza 
+3. Salvo il prodotto
+4. Incremento l'indice
+5. Decremento la lunghezza 
+
 **Output**
-1. Stampare i prodotti della lista
+
+1. Stampare i prodotti della lista nell'html
